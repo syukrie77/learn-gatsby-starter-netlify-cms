bdfd5beefc2d6444e0e8dbb2725c8a92e503db55
Muhammad Syukri <39161232+syukrie77@users.noreply.github.com>
2023-01-06 13:01:35 +0000

    edit logo logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Aretanet"
             style={{ width: "8em", height: "8em" }}
           />
         </div>
