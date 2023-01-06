2d62ee7e85594989025a3023d70bd158eb5b88a0
syukrie77 <syukrie77@gmail.com>
2023-01-06 18:48:30 +0700

    update landingpage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import logo from "../img/logo.svg";
+//import logo from "../img/logo.svg";
+import logo from "../img/logo-areta.png";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
