4ef920b86b1df7b70e9f9a5f5ab8955434d1ad9e
Muhammad Syukri <39161232+syukrie77@users.noreply.github.com>
2023-01-06 13:15:42 +0000

    custom size logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "66px" }} />
+              <img src={logo} alt="Aretanet" style={{ width: "66px" height: "66px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
