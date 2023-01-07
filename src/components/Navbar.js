31a11447bc2db376bd3816bcec44b5c23dff4767
Muhammad Syukri <39161232+syukrie77@users.noreply.github.com>
2023-01-07 07:39:32 +0000

    update size loogo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Aretanet" style={{ width: "38px" }} />
+              <img src={logo} alt="Aretanet" style={{ width: "37px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
